de4304b921f061d07205743702e551915bb2e3a8
snljty <snljty@gmail.com>
2020-12-17 22:16:45 +0800

    add new comment

--- test.c
+++ test.c
@@ -1,4 +1,6 @@
-/*  This file uses fortran function read_double_from_string_  */
+/**************************************************************
+ *  This file uses Fortran function read_double_from_string_  *
+ **************************************************************/
 
 # ifdef __cplusplus
 extern "C" {
@@ -7,6 +9,11 @@ extern "C" {
 # include <stdio.h>
 # include <string.h>
 
+/*  note  */
+/*  for most compilers, Fortran will append a '_' to the name of the function,  */
+/*  and uses only lower letters in name of functions.  */
+/*  also, please remember that Fortran passes addresses of arguments, but C/C++ passes values,  */
+/*  hence if you want to call a Fortran function in a C program, you have to pass pointers for all.  */
 void read_double_from_string_(const int *const str_length, const char *const read_str, double *d_number);
 
 int main()
